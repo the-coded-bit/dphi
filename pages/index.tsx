f4c1ea498e1d9234b90ccba682543f65a8a88dbe
Arpit Gupta <theburningdesiresince2002@gmail.com>
2022-10-07 19:12:31 +0530

    NextJs & Typescript template

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Default App" />
+      </Head>
+      
+      <main>Hello!!!</main>
+
+    </div>
+  )
+}
