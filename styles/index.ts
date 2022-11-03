00be9a677b970bbc77b13f9a1997a9d38866e05b
Arpit Gupta <theburningdesiresince2002@gmail.com>
2022-11-03 19:29:24 +0530

    style: change, add styles details

--- styles/index.ts
+++ styles/index.ts
@@ -4,3 +4,4 @@ export { default as statsStyles } from './StatsBanner.module.css';
 export { default as statStyles } from './Stats.module.css';
 export { default as homeStyles } from './Home.module.css';
 export { default as createStyles } from './CreateChallenge.module.css';
+export { default as cardStyles } from './Card.module.css';
